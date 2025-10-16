d8c75ea33786705759abdab460c3333164809c20
IotaSphere <40618095+Geocube101@users.noreply.github.com>
2025-10-16 17:07:07 -0400

    Add files via upload
    Added IBAN_CODE detection

--- test_team_code7.py
+++ test_team_code7.py
@@ -1,6 +1,8 @@
 """Unit test file for team code7"""
 import unittest
-from pii_scan import analyze_text, show_aggie_pride  # noqa 
+
+from presidio_anonymizer import RecognizerResult
+from pii_scan import analyze_text, show_aggie_pride  # noqa
 
 
 class TestTeam_code7(unittest.TestCase):
@@ -20,6 +22,42 @@ class TestTeam_code7(unittest.TestCase):
 
     def test_iban_code(self):
         """Test IBAN_CODE functionality"""
+
+        # Assemble IBAN Codes
+        positive_ibans: list[str] = [
+            'IE64 IRCE 9205 0112 3456 78',
+            'GB82 WEST 1234 5698 7654 32',
+            'DE02 2022 0800 0051 0663 66',
+            'GB68 TRWI 2314 7094 9392 79',
+            'FR14 2004 1010 0505 0001 3M02606'
+        ]
+
+        negative_ibans: list[str] = [
+            'ZZ91 2100 0418 4502 0005 1332',
+            'SA03 8000 0000 6080 1016 7520',
+            'PL84 1090 1014 0000 0712 1981 2874',
+            'IF64 IRFD 9205 0112 3456 78',
+            'DE89 3704 2398 0532 0130 00'
+        ]
+
+        # Positive Test Cases
+        print('Positive IBAN Test Cases:')
+
+        for iban in positive_ibans:
+            iban: str = f'My IBAN is {iban}'
+            print(f' ... {iban}')
+            result: list[RecognizerResult] = analyze_text(iban, ['IBAN_CODE'])
+            self.assertGreater(len(result), 0)
+            self.assertEqual(result[0].entity_type, 'IBAN_CODE')
+
+        # Negative Test Cases
+        print('\nNegative IBAN Test Cases:')
+
+        for iban in negative_ibans:
+            iban: str = f'My IBAN is {iban}'
+            print(f' ... {iban}')
+            result: list[RecognizerResult] = analyze_text(iban, ['IBAN_CODE'])
+            self.assertEqual(len(result), 0)
 
     def test_ip_address(self):
         """Test IP_ADDRESS functionality"""
