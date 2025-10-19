28d35a60ffc908a57f4ebcfc1301296790892952
dlallen04 <dlallen4@aggies.ncat.edu>
2025-10-19 14:03:50 +0000

    Completed IN_VEHICLE_REGISTRATION test case

--- test_team_seven_eleven.py
+++ test_team_seven_eleven.py
@@ -20,6 +20,29 @@ class TestTeam_seven_eleven(unittest.TestCase):
 
     def test_in_vehicle_registration(self):
         """Test IN_VEHICLE_REGISTRATION functionality"""
+        # Parts of vehicle registration
+        state_code = {"MH", "JK", "MZ", "DL"}
+        rto_number = {"01", "14", "05", "29"}
+        registration_series = {"A", "HK", "GR", "BN"}
+        unique_number = {"0345", "4132", "0023", "9026"}
+
+        # Build vehicle registration
+        # Positive test cases
+        for s in state_code:
+            for rto in rto_number:
+                for series in registration_series:
+                    for num in unique_number:
+                        vehicle_registration_text = f'My vehicle registration number is {s}{rto}{series}{num}'
+                        result = analyze_text(vehicle_registration_text, ['IN_VEHICLE_REGISTRATION']) 
+                        # Check entity_type for IN_VEHICLE_REGISTRATION
+                        self.assertEqual(result[0].entity_type, 'IN_VEHICLE_REGISTRATION')
+
+        # Negative test cases
+        result = analyze_text('My vehicle registration is hidden ', ['IN_VEHICLE_REGISTRATION'])
+        self.assertListEqual(result, [])
+
+        result = analyze_text('My vehicle registration is 111-222-3333 ', ['IN_VEHICLE_REGISTRATION'])
+        self.assertListEqual(result, [])
 
     def test_in_voter(self):
         """Test IN_VOTER functionality"""
